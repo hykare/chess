ce129ca955b038bfdfadeba78dc24835e837a72a
hykare <78385964+hykare@users.noreply.github.com>
2022-08-20 13:11:07 +0200

    parse moves from string

--- lib/move.rb
+++ lib/move.rb
@@ -5,4 +5,12 @@ class Move
     @from = from
     @to = to
   end
+
+  def self.parse(string)
+    # string in the form 'd5 to e6'
+    moves = string.split('to')
+    from = Position.parse(moves.first.strip)
+    to = Position.parse(moves.last.strip)
+    new(from, to)
+  end
 end
