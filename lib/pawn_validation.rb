e5119288248086c875c21ffeaad650a8de19a5e8
hykare <78385964+hykare@users.noreply.github.com>
2022-08-20 12:19:53 +0200

    pawn validation test scaffolding, basic implementation

--- lib/pawn_validation.rb
+++ lib/pawn_validation.rb
@@ -1,7 +1,10 @@
 class PawnValidation < Validation
   def piece_move_valid?
-    @message = "this piece can't move this way"
+    @message = "this piece can't move this way\n"
 
-    true
+    move_one = (move.from.file == move.to.file) && (move.to.rank == move.from.rank + 1)
+    move_two = (move.from.file == move.to.file) && (move.from.rank == 2) && (move.to.rank == 4)
+
+    move_one || move_two
   end
 end
