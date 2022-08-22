3a256248b31c0daed1ddd177bbdd7079b57b067b
hykare <78385964+hykare@users.noreply.github.com>
2022-08-22 16:32:03 +0200

    delete unused validation subclasses

--- lib/bishop_validation.rb
+++
@@ -1,7 +0,0 @@
-class BishopValidation < Validation
-  def piece_move_valid?
-    @message = "this piece can't move this way\n"
-
-    move.diagonal?
-  end
-end
--- lib/king_validation.rb
+++
@@ -1,7 +0,0 @@
-class KingValidation < Validation
-  def piece_move_valid?
-    @message = "this piece can't move this way\n"
-
-    move.distance.one?
-  end
-end
