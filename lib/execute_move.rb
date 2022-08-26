d8bb48d9e319a4107b1ef7f115e5377a29f366f8
hykare <78385964+hykare@users.noreply.github.com>
2022-08-26 21:27:49 +0200

    execute castling

--- lib/execute_move.rb
+++ lib/execute_move.rb
@@ -9,6 +9,8 @@ class ExecuteMove
   def self.for(board, move)
     if en_passant?(board, move)
       ExecuteEnPassant
+    elsif castling?(board, move)
+      ExecuteCastling
     else
       ExecuteMove
     end.new(board, move).execute
@@ -20,19 +22,42 @@ class ExecuteMove
     board.piece_at(move.to).nil?
   end
 
+  private_class_method def self.castling?(board, move)
+    board.piece_at(move.from).is_a?(King) &&
+    move.distance == 2
+  end
+
   def execute
+    board.piece_at(move.from).has_moved = true
     board.update(move)
     board.last_move = move
-    board.piece_at(move.from).has_moved = true
   end
 end
 
 class ExecuteEnPassant < ExecuteMove
   def execute
-    board.update(move)
-    board.last_move = move
+    super
 
     enemy_pawn_position = Position.new(move.from.rank, move.to.file)
     board.state[enemy_pawn_position] = nil
+  end
+end
+
+class ExecuteCastling < ExecuteMove
+  def execute
+    super
+
+    board.update(rook_move)
+  end
+
+  def rook_move
+    rank = move.from.rank
+    rook_start_file = move.from < move.to ? 'h' : 'a'
+    rook_target_file = move.from < move.to ? 'f' : 'd'
+
+    rook_start_position = Position.new(rank, rook_start_file)
+    rook_target_position = Position.new(rank, rook_target_file)
+
+    Move.for(rook_start_position, rook_target_position)
   end
 end
